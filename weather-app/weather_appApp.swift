c5475923b088d696f324ea38c4f32f4e7e4dc81b
Mandip Chaudhary <consultant-mchaudhary@remine.com>
2022-03-27 17:16:06 +0545

    Initial Commit

---
+++ weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by Mandip Chaudhary on 27/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
